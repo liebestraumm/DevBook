a8b3c1439e34b97222181c5907c7767aac1b5ba4
Carlos Agelvis <carlosarturo.agelvisvergara@connect.qut.edu.au>
2020-12-18 19:57:10 +1000

    Added routes and db connection file

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
+import express from 'express';
+import connectDB from './config/db.js';
+import usersRoute from './routes/api/users.js';
+import authRoute from './routes/api/auth.js';
+import postsRoute from './routes/api/posts.js';
+import profileRoute from './routes/api/profile.js';
+
 const app = express();
+
+//Connect Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
      res.send("API!");
-})
+});
+
+//Define Routes
+app.use('/api/users', usersRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/posts', postsRoute);
 
 app.listen(PORT, () => {
     console.log(`Server works in ${PORT}`);
