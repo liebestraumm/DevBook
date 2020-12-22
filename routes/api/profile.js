9461e5f01be0e8efd9c2bc76643471abe4e129a3
Carlos Agelvis <carlosarturo.agelvisvergara@connect.qut.edu.au>
2020-12-22 17:08:33 +1000

    Added api/profile Endpoint (POST) to create profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import auth from '../../middleware/auth.js';
 import Profile from '../../models/Profile.js';
+import { check, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -27,5 +28,58 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+//@route        POST api/profile
+//@desc         Create or Update user Profile
+//@access       Private
+
+router.post('/', [auth, [
+    check('status', "Status is Required").not().isEmpty(),
+    check('skills', "Skills are required").not().isEmpty()
+]], async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagran,
+        linkedin
+    } = req.body
+
+    //Build profile object
+    const profileFields = {}
+    profileFields.user = req.user.id;
+
+    //Check if fields values have been introduced by the user
+    if(company) profileFields.company = company
+    if(website) profileFields.website = website
+    if(location) profileFields.location = location
+    if(bio) profileFields.bio = bio
+    if(status) profileFields.status = status
+    if(githubusername) profileFields.githubusername = githubusername
+    if(skills) {
+        profileFields.skills = skills.split(',').map((skill) => 
+            skill.trim()
+        )
+    }
+
+    console.log(profileFields.skills)
+
+    res.send('Hello');
+    
+})
 
 export default router;
